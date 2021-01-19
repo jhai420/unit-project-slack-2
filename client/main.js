6882b6f49e8e2df5f35d9b39cf8e409494f64742
Jenny Hai <jennyhai@Jennys-MacBook-Pro.local>
2021-01-19 12:59:39 -0800

    calendar complete

--- client/main.js
+++ client/main.js
@@ -1,9 +1,56 @@
 // The following code appends a title to the page
 // document.createElement creates an element that can be altered and then inserted into the DOM
 // document.body.appendChild places a node as a child under the body element
-const title = document.createElement('div');
-title.innerHTML = 'Social Calendar';
-document.body.appendChild(title);
 
 // Your schedule can be accessed through the global object "schedule"
 console.log(schedule);
+
+function parseUnitsbyWeek(array) {
+  let units = {};
+
+  array.forEach(unit => {
+    let week = unit.week;
+    let day = unit.day;
+    if (unit.week === 1) {
+      if (!units[week]) units[week] = {};
+      units[week][day] = unit;
+    } else if (unit.week === 2) {
+      if (!units[week]) units[week] = {};
+      units[week][day] = unit;
+    } else if (unit.week === 3) {
+      if (!units[week]) units[week] = {};
+      units[week][day] = unit;
+    }
+  })
+  return units;
+}
+
+function renderUnits() {
+  let DAYSOFWEEK = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  let unitsByWeek = parseUnitsbyWeek(schedule);
+  for (let week in unitsByWeek) {
+    DAYSOFWEEK.forEach(weekday => {
+      let weeklyUnits = unitsByWeek[week];
+
+      if (weeklyUnits[weekday]) {
+        let challenge = weeklyUnits[weekday].challenge;
+        let event = document.createElement('div');
+        let goals = weeklyUnits[weekday].goals
+        let text = challenge + '<br>Goals: ';
+        goals.forEach(goal => text += goal)
+
+        event.innerHTML = text;
+        event.setAttribute('class', 'event');
+        document.getElementById('wrapper').appendChild(event);
+      } else {
+        let event = document.createElement('div');
+        event.innerHTML = 'no events scheduled';
+        event.setAttribute('class', 'event');
+        document.getElementById('wrapper').appendChild(event);
+      }
+    })
+  }
+}
+
+renderUnits();
+
